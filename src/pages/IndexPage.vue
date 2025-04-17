4867d739d25be583d8359500b1a00d2e7929d24c
ilogic-8-a <urypin2010@gmail.com>
2025-04-17 17:55:27 +0300

    /

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,52 @@
 <template>
   <q-page class="flex flex-center">
-    <button>Сделать по приколу</button>
-    <input placeholder="Написать по приколу">
-    <div class="gaga">
-      <button>Добавить по приколу</button>
-    </div>
-    <apexchart type="bar" height="350" :options="{ xaxis: { categories: ['test1', 'test2']}}" :series="[{ name: 'test', data: [ 30, 40]}]"/>
+    <apexchart type="bar" height="350" width='500' :options="options" :series="series"/>
+    <apexchart type="donut"  width='500' :options="options2" :series="series2"/>
   </q-page>
 
 </template>
 
 <script setup>
+import {ref } from 'vue';
 
+
+let series = ref([
+  {
+    name: 'test',
+    data: [ 30, 40,50,43,65,84,14,98,35]
+  }
+  ]
+)
+
+let options= ref({ xaxis: { categories: ['test1', 'test2', 'test3','test4','test5','test6','test7','test8','test9',]}})
+
+let series2 = ref([50, 23, 11, 99, 48])
+
+let options2 = ref({
+  chart: {
+    type: 'donut',
+  },
+  labels: ['test-1','test-2','test-3','test-4','test-5',],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 500
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }],
+  colors: ['orange', 'green', 'blue', 'red', 'purple'],
+  dataLabels: {
+    enabled: true
+  },
+  legend: {
+    position: 'right',
+    offsetY: 0,
+    height: 230,
+  }
+}
+)
 </script>
