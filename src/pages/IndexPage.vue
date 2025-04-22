60f6100a6b619814f3e3cbb714fe12ca7bc0c950
ilogic-7-a <daniilr96@gmail.com>
2025-04-22 19:50:42 +0500

    f

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,73 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+
+
+
+
+
+
+
+
+    <q-card bordered class="bg-dark text-white my-card">
+      <q-card-section>
+        <div class="text-h5">Входящие звонки</div>
+        <div class="text-number1">5</div>
+        <div class="text-subtitle1">за неделю</div>
+      </q-card-section>
+      <q-card class="bg-dark text-white my-card">
+        <q-card-section>
+          <div class="text-h5">Входящие звонки</div>
+          <div class="text-number2">5</div>
+          <div class="text-subtitle2">за неделю</div>
+        </q-card-section>
+        <q-card class="bg-dark text-white my-card">
+          <q-card-section>
+            <div class="text-h5">Входящие звонки</div>
+            <div class="text-number3">5</div>
+            <div class="text-subtitle3">за неделю</div>
+          </q-card-section>
+          <q-card class="bg-dark text-white my-card">
+          <q-card-section>
+            <div class="text-h5">Входящие звонки</div>
+            <div class="text-number4">5</div>
+            <div class="text-subtitle4">за неделю</div>
+          </q-card-section>
+            </q-card>
+        </q-card>
+      </q-card>
+
+
+
+
+
+
+        <q-separator dark inset />
+
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+    <q-card flat bordered class="my-card">
+
+
+
+
+    </q-card>
+
+
 </template>
 
 <script setup>
-//
+
 </script>
+
+<style scoped>
+.text-number1 {
+  font-size: 32px;
+  font-weight: bold;
+}
+.bg-grey-9  {
+  font-size:30px;
+
+}
+</style>
