e2303c7833b7019b9ef12162860fe7bb0636c42a
ilogic-8-a <urypin2010@gmail.com>
2025-04-22 18:00:40 +0300

    /

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,52 +1,117 @@
 <template>
-  <q-page class="flex flex-center">
-    <apexchart type="bar" height="350" width='500' :options="options" :series="series"/>
-    <apexchart type="donut"  width='500' :options="options2" :series="series2"/>
-  </q-page>
+  <div class="row q-gutter-md">
+    <div>
+      <div class="row q-gutter-md">
+        <q-card
+          class="my-card text-white"
+          style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+        >
+          <q-card-section class="card">
+            <div class="text-h6">ПРОСРОЧЕННЫЕ ЗАДАЧИ</div>
+            <div class="textnumber">65</div>
+            <div class="textnumber2">0</div>
+            <div class="text-subtitle2">за неделю</div>
+          </q-card-section>
+        </q-card>
+        <q-card
+          class="my-card text-white"
+          style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+        >
+          <q-card-section class="card">
+            <div class="text-h6">ВЫПОЛНЕННЫЕ ЗАДАЧИ</div>
+            <div class="textnumber">0</div>
+            <div class="text-subtitle2">за неделю</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="row q-gutter-md">
+      <q-card
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+        <q-card-section class="card">
+          <div class="text-h6">ЗАДАЧИ К ВЫПОЛНЕНИЮ</div>
+          <div class="textnumber">75</div>
+          <div class="textnumber2">+8</div>
+          <div class="text-subtitle2">за неделю</div>
+        </q-card-section>
+      </q-card>
+      <q-card
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+        <q-card-section class="card">
+          <div class="text-h6">СДЕЛОК БЕЗ ЗАДАЧ</div>
+          <div class="textnumber">3506</div>
+          <div class="textnumber2">+8</div>
+          <div class="text-subtitle2">за неделю</div>
+        </q-card-section>
+      </q-card>
+    </div>
+    </div>
+    <div class="row q-gutter-md">
+      <q-card
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+
+    <q-card-section class="card">
+      <div class="text-h6">ВЫПОЛНЕННЫЕ ЗАДАЧИ</div>
+      <div class="textnumber">0</div>
+      <div class="text-subtitle2">за неделю</div>
+    </q-card-section>
+      </q-card>
+    </div>
+  </div>
+
+
+
+
 
 </template>
+<!--  <q-page class="flex flex-center">-->
+<!--    <apexchart type="bar" height="350" width='500' :options="options" :series="series"/>-->
+<!--    <apexchart type="donut"  width='500' :options="options2" :series="series2"/>-->
+<!--  </q-page>-->
+
+
 
 <script setup>
-import {ref } from 'vue';
+
+import { ref } from 'vue';
 
 
-let series = ref([
-  {
-    name: 'test',
-    data: [ 30, 40,50,43,65,84,14,98,35]
-  }
-  ]
-)
 
-let options= ref({ xaxis: { categories: ['test1', 'test2', 'test3','test4','test5','test6','test7','test8','test9',]}})
 
-let series2 = ref([50, 23, 11, 99, 48])
-
-let options2 = ref({
-  chart: {
-    type: 'donut',
-  },
-  labels: ['test-1','test-2','test-3','test-4','test-5',],
-  responsive: [{
-    breakpoint: 480,
-    options: {
-      chart: {
-        width: 500
-      },
-      legend: {
-        position: 'bottom'
-      }
-    }
-  }],
-  colors: ['orange', 'green', 'blue', 'red', 'purple'],
-  dataLabels: {
-    enabled: true
-  },
-  legend: {
-    position: 'right',
-    offsetY: 0,
-    height: 230,
-  }
-}
-)
+// let series = ref([
+//   {
+//     name: 'test',
+//     data: [ 30, 40,50,43,65,84,14,98,35]
+//   }
+//   ]
+// )
+//
+// let options= ref({ xaxis: { categories: ['test1', 'test2', 'test3','test4','test5','test6','test7','test8','test9',]}})
+//
 </script>
+
+
+
+<style scoped>
+.text-h6 {
+  font-weight: bold;
+  font-size: 10pt;
+}
+
+.text-subtitle2 {
+  font-size: 10pt;
+}
+
+.textnumber {
+  font-size: 20pt;
+  opacity: 0.5;
+}
+
+.card {
+}
+</style>
