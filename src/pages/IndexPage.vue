9d0a78f6cd49770bb788b9c915dbcf757a1cf4f6
ilogic-8-a <urypin2010@gmail.com>
2025-04-29 18:01:25 +0300

    /

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,6 +9,9 @@
           <q-card-section class="card">
             <div class="text-h6">ПРОСРОЧЕННЫЕ ЗАДАЧИ</div>
             <div class="textnumber">65</div>
+            <div style="width: 50%;">
+                 <hr>
+            </div>
             <div class="textnumber2">0</div>
             <div class="text-subtitle2">за неделю</div>
           </q-card-section>
@@ -32,6 +35,9 @@
         <q-card-section class="card">
           <div class="text-h6">ЗАДАЧИ К ВЫПОЛНЕНИЮ</div>
           <div class="textnumber">75</div>
+          <div style="width: 50%;">
+            <hr>
+          </div>
           <div class="textnumber2">+8</div>
           <div class="text-subtitle2">за неделю</div>
         </q-card-section>
@@ -43,6 +49,10 @@
         <q-card-section class="card">
           <div class="text-h6">СДЕЛОК БЕЗ ЗАДАЧ</div>
           <div class="textnumber">3506</div>
+          <div class="textnumber3">50 000р</div>
+          <div style="width: 50%;">
+            <hr>
+          </div>
           <div class="textnumber2">+8</div>
           <div class="text-subtitle2">за неделю</div>
         </q-card-section>
@@ -56,9 +66,10 @@
       >
 
     <q-card-section class="card">
-      <div class="text-h6">ВЫПОЛНЕННЫЕ ЗАДАЧИ</div>
-      <div class="textnumber">0</div>
-      <div class="text-subtitle2">за неделю</div>
+      <div class="text-h6">ИСТОЧНИКИ СДЕЛОК</div>
+      <div class="flex flex-center">
+        <apexchart type="radialBar"  width='325' :options="options2" :series="series2"/>
+      </div>
     </q-card-section>
       </q-card>
     </div>
@@ -69,10 +80,7 @@
 
 
 </template>
-<!--  <q-page class="flex flex-center">-->
-<!--    <apexchart type="bar" height="350" width='500' :options="options" :series="series"/>-->
-<!--    <apexchart type="donut"  width='500' :options="options2" :series="series2"/>-->
-<!--  </q-page>-->
+
 
 
 
@@ -83,16 +91,55 @@ import { ref } from 'vue';
 
 
 
-// let series = ref([
-//   {
-//     name: 'test',
-//     data: [ 30, 40,50,43,65,84,14,98,35]
-//   }
-//   ]
-// )
-//
-// let options= ref({ xaxis: { categories: ['test1', 'test2', 'test3','test4','test5','test6','test7','test8','test9',]}})
-//
+let series2 = ref([100,100])
+
+let options2= ref({
+  chart: {
+    height: 390,
+    type: 'radialBar',
+  },
+  plotOptions: {
+    radialBar: {
+      offsetY: 0,
+      startAngle: 0,
+      endAngle: 270,
+      hollow: {
+        margin: 5,
+        size: '30%',
+        background: 'transparent',
+        image: undefined,
+      },
+      dataLabels: {
+        name: {
+          show: false,
+        },
+        value: {
+          show: false,
+        }
+      },
+      barLabels: {
+        enabled: true,
+        useSeriesColors: true,
+        offsetX: -8,
+        fontSize: '16px',
+        formatter: function(seriesName, opts) {
+          return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+        },
+      },
+    }
+  },
+  colors: ['#0084ff', '#0084ff'],
+  labels  : ['ИНТЕГРАЦИИ С САЙТОМ ВДЕЛО', 'ВИРТУАЛЬНАЯ АТС МЕГАФОН'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+        show: false
+      }
+    }
+  }]
+})
+
 </script>
 
 
@@ -110,6 +157,15 @@ import { ref } from 'vue';
 .textnumber {
   font-size: 20pt;
   opacity: 0.5;
+}
+
+.textnumber2 {
+  color:limegreen;
+  opacity: 0.5;
+}
+
+.textnumber3 {
+  font-size: 10pt;
 }
 
 .card {
