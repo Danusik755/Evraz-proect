ad19a161f605e6d023252f6ff9a4cbcc8b61751a
ilogic-8-a <urypin2010@gmail.com>
2025-05-06 17:58:34 +0300

    /

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -49,7 +49,7 @@
           <q-card-section class="card">
             <div class="text-h6">СДЕЛОК БЕЗ ЗАДАЧ</div>
             <div class="textnumber">3506</div>
-            <div class="textnumber3">50 000р</div>
+            <div class="textnumber3">50 000 ₽</div>
             <div style="width: 50%;">
               <hr>
             </div>
@@ -73,41 +73,76 @@
         </q-card-section>
       </q-card>
     </div>
+    <div class="row q-gutter-md">
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section
+        style="width: 500px"
+        class="card">
+        <div class="text-h6">СДЕЛКИ ПО МЕНЕДЖЕРАМ</div>
+        <div class="textnumber">3516</div>
+        <div class="textnumber3">50 000 ₽</div>
+        <div style='width: 50%;'>
+          <hr>
+        </div>
+        <div class="row">
+          <div class="textnumber6">Admin CRM</div>
+          <div style="font-weight: bold">3055 сделок</div>
+          <div
+            style="opacity: 0.3"
+            class="textnumber3">(50 000 ₽)</div>
+          <div class="textnumber6">Иван Седых</div>
+          <div style="font-weight: bold">151 сделок</div>
+          <div
+            style="opacity: 0.3"
+            class="textnumber3">(50 000 ₽)</div>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
+  </div>
+
+
+
   <div class="row justify-center">
     <q-card bordered class="bg-dark text-white my-card">
-      <q-card-section>
-        <div class="text-h5">Входящие звонки</div>
+      <q-card-section style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <div class="text-h5">ВХОДЯЩИЕ ЗВОНКИ</div>
         <div class="text-number1">5</div>
         <div class="text-subtitle3">за неделю</div>
       </q-card-section>
     </q-card>
-    <q-card class="bg-dark text-white my-card">
-      <q-card-section>
-        <div class="text-h5">исходящие звонки</div>
+    <q-card
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      class="bg-dark text-white my-card">
+      <q-card-section style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <div class="text-h5">ИСХОДЯЩИЕ ЗВОНКИ</div>
         <div class="text-number2">5</div>
         <div class="text-subtitle4">за неделю</div>
       </q-card-section>
     </q-card>
-    <q-card class="bg-dark text-white my-card">
-      <q-card-section>
-        <div class="text-h5">примечаний</div>
+    <q-card
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      class="bg-dark text-white my-card">
+      <q-card-section style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <div class="text-h5">ПРИМЕЧАНИЙ</div>
         <div class="text-number3">5</div>
         <div class="text-subtitle3">за неделю</div>
       </q-card-section>
     </q-card>
 
     <q-card   class="bg-dark text-white my-card">
-      <q-card-section>
-
-        <div class="text-h5">успешные сделки</div>
-        <div class="text-number4">3  082</div>
+      <q-card-section style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <div class="text-h5">УСПЕШНЫЕ СДЕЛКИ</div>
+        <div class="text-number4">3082</div>
         <div class="text-subtitle4"></div>
-        <div style="font-size:25px">0 р</div>
+        <div style="font-size:25px">0р</div>
         <div style="width: 50%;">
           <hr>
         </div>
-        <div  class="text-number1 "  style="font-size: 30px">+10</div>
+        <div  class="text-number10 "  style="font-size: 30px">+10</div>
         <div class="text-subtitle1">за неделю</div>
       </q-card-section>
     </q-card>
@@ -171,17 +206,19 @@ let options2= ref({
         enabled: true,
         useSeriesColors: true,
         offsetX: -8,
-        fontSize: '16px',
+        fontSize: '8px',
+
+        fontWeight: 600,
         formatter: function(seriesName, opts) {
           return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
         },
       },
     }
   },
-  colors: ['#0084ff', '#0084ff'],
+  colors: ['blue', 'blue'],
   labels  : ['ИНТЕГРАЦИИ С САЙТОМ ВДЕЛО', 'ВИРТУАЛЬНАЯ АТС МЕГАФОН'],
   responsive: [{
-    breakpoint: 480,
+    breakpoint: 200,
     options: {
       legend: {
         show: false
@@ -192,37 +229,47 @@ let options2= ref({
 </script>
 
 <style scoped>
-.text-number1 {
+.text-number10 {
   font-size: 32px;
   font-weight: bold;
   flex-grow: revert;
-  opacity: 20%;
+  opacity: 0.5;
+  color:limegreen
 }
 .text-number4 {
-  opacity: 20%;
+  opacity: 0.5;
+
 }
 .text-number3 {
-  opacity: 20%;
+  opacity: 0.5;
+
 }
 .text-number2 {
-  opacity: 20%;
+  opacity: 0.5;
 }
 .text-number1 {
-  opacity: 20%;
+  opacity: 0.5;
+
 }
 .bg-dark {
-  //line-height: 120px;
   font-size: 40px;
-
+  color: limegreen;
   margin-top: 60px;
   width: 320px;
   margin-right: 10px;
   align-items: center;
+
+
 }
 .div {
-    opacity: 20% ;
+    opacity: 0.5 ;
   }
 .text-h6 {
+  font-weight: bold;
+  font-size: 10pt;
+}
+
+.text-h5 {
   font-weight: bold;
   font-size: 10pt;
 }
