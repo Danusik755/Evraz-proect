ac52e823dac62ea6fc8e25e29c4fb0439a417739
ilogic-8-a <urypin2010@gmail.com>
2025-05-20 18:00:58 +0300

    /

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -87,6 +87,13 @@
         class="card">
         <div class="text-h6">СДЕЛКИ ПО МЕНЕДЖЕРАМ</div>
         <div class="textnumber">3516</div>
+        <div class="row justify-center items-start q-gutter-md">
+          <div
+            style="margin-left: 375px"
+            class="textnumber2">+8
+          </div>
+          <div class="row-q-gutter-xs">за неделю</div>
+        </div>
         <div
           style="font-weight: bold"
           class="textnumber11">50 000 ₽
@@ -103,6 +110,9 @@
             style="opacity: 0.3"
             class="textnumber3">(50 000 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 220px">+6</div>
           <q-linear-progress :value="1" color="yellow" class="line"></q-linear-progress>
         </div>
         <div
@@ -114,6 +124,9 @@
             style="opacity: 0.3"
             class="textnumber3">(0 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 263px">0</div>
           <q-linear-progress :value="0.04" color="yellow" class="line"></q-linear-progress>
         </div>
         <div
@@ -125,6 +138,9 @@
             style="opacity: 0.3"
             class="textnumber3">(0 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 196px">+2</div>
           <q-linear-progress :value="0.04" color="yellow" class="line"></q-linear-progress>
         </div>
         <div
@@ -136,6 +152,9 @@
             style="opacity: 0.3"
             class="textnumber3">(0 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 220px">0</div>
           <q-linear-progress :value="0.02" color="yellow" class="line"></q-linear-progress>
         </div>
         <div
@@ -147,6 +166,9 @@
             style="opacity: 0.3"
             class="textnumber3">(0 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 224px">0</div>
           <q-linear-progress :value="0.01" color="yellow" class="line"></q-linear-progress>
         </div>
         <div
@@ -158,6 +180,9 @@
             style="opacity: 0.3"
             class="textnumber3">(0 ₽)
           </div>
+          <div
+            class="textnumber2"
+            style="margin-left: 303px">0</div>
           <q-linear-progress :value="0.008" color="yellow" class="line"></q-linear-progress>
         </div>
       </q-card-section>
@@ -183,26 +208,40 @@
         style="width: 400px"
         class="card">
         <div
-          class="text-h6">ИСПОЛЬЗОВАНИЕ СИСТЕМЫ ЗА НЕДЕЛЮ</div>
+          class="text-h6">ИСПОЛЬЗОВАНИЕ СИСТЕМЫ ЗА НЕДЕЛЮ
+        </div>
         <div
           style="display: flex; justify-content: flex-end"
           class="textnumber20">За неделю
         </div>
+        <div
+          style="margin-top: 20px"
+          class="row q-gutter-xs">
+          <img
+            style="width:30px"
+            src="https://avatars.mds.yandex.net/get-socsnippets/12856884/2a0000019158c1fcb75dc89d8e6c6fec8753/square_83">
+          <div>Admin CRM</div>
           <div
-            style="margin-top: 20px"
-            class="row q-gutter-xs">
-        <div>Admin CRM</div>
-        <div
-          style="font-weight: bold"
-          class="row q-gutter-xs"></div>
-        <div  style="font-weight: bold">0 м.</div>
-        <q-linear-progress :value="0" color="yellow" class="line"></q-linear-progress>
-        <div  class="row q-gutter-xs">Валерий Богинский</div>
-        <div
-          style="font-weight: bold"
-          class="row q-gutter-xs">0 м.</div>
-        <q-linear-progress :value="0" color="yellow" class="line"></q-linear-progress>
+            style="font-weight: bold"
+            class="row q-gutter-xs"></div>
+          <div style="font-weight: bold">0 м.</div>
+          <div
+            class="text11">18 м.
           </div>
+          <q-linear-progress :value="0" color="yellow" class="line"></q-linear-progress>
+          <img
+            style="width:30px"
+            src="https://avatars.mds.yandex.net/i?id=9f64755eec4927fe8ba29cbd5d1a3749d82fcdae-3590262-images-thumbs&n=13">
+          <div class="row q-gutter-xs">Валерий Богинский</div>
+          <div
+            style="font-weight: bold"
+            class="row q-gutter-xs">0 м.
+          </div>
+          <div
+            class="text12">10 м.
+          </div>
+          <q-linear-progress :value="0" color="black" class="line"></q-linear-progress>
+        </div>
       </q-card-section>
     </q-card>
     <q-card
@@ -211,9 +250,81 @@
     >
       <q-card-section
         style="width: 400px"
-        class="card">
+        class="row-q-gutter-xs">
         <div class="text-h6">ПОСЛЕДНИЕ ФАЙЛЫ</div>
-        <div class="textnumber9">⚠ Недостаточно данных для отображения</div>
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm"
+          >
+          Доходы "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs">
+        </div>
+
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm"
+          >
+          Задачи "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs">
+        </div>
+
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm">
+          Работники "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs"></div>
+
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm"
+          >
+          Важная информация "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs"></div>
+
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm"
+          >
+          Секреты "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs"></div>
+
+        <div class="row q-mb-md">
+          <img
+            style="width: 50px"
+            src="https://avatars.mds.yandex.net/i?id=85e05f3c027cadf9e1d8a614850ed524a9a8bd66-9068341-images-thumbs&n=13"
+            class="q-mr-sm"
+          >
+          База данных "ЕВРАЗ"
+        </div>
+        <div
+          style="font-weight: bold"
+          class="row q-gutter-xs">
+        </div>
       </q-card-section>
     </q-card>
     <q-card bordered class="bg-dark text-white my-card">
@@ -255,6 +366,7 @@
         <div class="text-subtitle1">за неделю</div>
       </q-card-section>
     </q-card>
+
 
     <q-card flat class="my-card">
       <q-card-section>
@@ -325,7 +437,7 @@ let options2 = ref({
   }]
 })
 
-let series3 = ref([100, 100,100,100,100])
+let series3 = ref([100, 100, 100, 100, 100])
 
 let options3 = ref({
   chart: {
@@ -378,6 +490,17 @@ let options3 = ref({
 </script>
 
 <style scoped>
+
+.text11 {
+  color: red;
+  margin-left: 185px;
+}
+
+.text12 {
+  color: red;
+  margin-left: 137px;
+}
+
 .line {
   border-radius: 10px;
 }
